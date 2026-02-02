771ced0b0d76c3dbefcd939d8ae317450be7857f
Nawf Abdullah <k.nawfabdullah@gmail.com>
2026-02-02 15:11:16 +0530

    chapter 15

--- chapter15/monoidExercises.hs
+++ chapter15/monoidExercises.hs
@@ -1,10 +1,14 @@
+import Control.Applicative (Alternative(empty))
+import Data.IntMap (member, empty)
 ------------------------- 1 ----------------------
 
 data Trivial = Trivial deriving (Eq, Show)
 instance Semigroup Trivial where
     _ <> _ = Trivial
 
-
+instance Monoid Trivial where
+    mempty = Trivial
+    mappend = (<>)
 ----------------- 2 -------------
 newtype Identity a = Identity a
 
@@ -12,53 +16,57 @@ newtype Identity a = Identity a
 instance Semigroup a => Semigroup (Identity a)  where
     (<>) (Identity a) (Identity b) = Identity (a <> b)
 
+instance Monoid a => Monoid (Identity a) where
+    mempty = Identity mempty
+    mappend = (<>)
 
 -------------- 3 ---------------
 data Two a b = Two a b
 instance (Semigroup a, Semigroup b) => Semigroup( Two a b) where 
     (<>) (Two a1 b1) (Two a2 b2) = Two (a1<>a2) (b1<>b2)
 
+instance (Monoid a, Monoid b) => Monoid (Two a b) where
+    mempty = Two mempty mempty
+    mappend = (<>)
+
 -------------- 4 -----------------
-data Three a b c = Three a b c
-instance (Semigroup a, Semigroup b,Semigroup c) => Semigroup( Three a b c) where 
-    (<>) (Three a1 b1 c1) (Three a2 b2 c2) = Three (a1<>a2) (b1<>b2) (c1<>c2)
 
---------------- 5 --------------------------
-data Four a b c d = Four a b c d
-instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup(Four a b c d) where 
-    (<>) (Four a1 b1 c1 d1) (Four a2 b2 c2 d2 ) = Four (a1<>a2) (b1<>b2) (c1<>c2) (d1<>d2)
-
---------------- 6 -------------------------
 newtype BoolConj = BoolConj Bool
 instance Semigroup BoolConj where 
     (<>) (BoolConj a) (BoolConj b) = BoolConj (a && b)
 
---------------- 7 ----------------------------
+
+instance Monoid BoolConj where
+    mempty = BoolConj True 
+    mappend = (<>)
+
+--------------- 5 ----------------------------
 
 newtype BoolDisj = BoolDisj Bool
 instance Semigroup BoolDisj where 
     (<>) (BoolDisj a) (BoolDisj b) = BoolDisj (a || b)
 
-
--------------- 8 -----------------------------
-
-data Or a b = Fst a | Snd b
-instance Semigroup (Or a b) where 
-    (<>) (Snd a) y = Snd a 
-    (<>) (Fst a) y = y
-
-
------------------- 9 ------------------------------
+instance Monoid BoolDisj where
+    mempty = BoolDisj False
+    mappend = (<>)
+-------------- 6  ------------------------------
 newtype Combine a b = Combine { unCombine :: (a -> b) }
 instance Semigroup b => Semigroup (Combine a b) where 
     (<>) (Combine f) (Combine g) = Combine (\x -> (f x) <> (g x)) 
 
------------------- 10 ---------------------------
+instance Monoid b => Monoid (Combine a b) where
+    mempty = Combine (\ _->mempty)
+    mappend = (<>)
+
+------------------ 7 ---------------------------
 
 newtype Comp a = Comp { unComp :: (a -> a) }
 instance Semigroup a => Semigroup (Comp a) where 
     (<>) (Comp f) (Comp g) = Comp (\x -> (f x) <> (g x)) 
 
+instance Monoid a => Monoid (Comp a) where 
+    mempty = Comp (\_ -> mempty)
+    mappend = (<>)
 ----------------- 11 ----------------------------
 
 data Validation a b = Failure a | Success b deriving (Eq, Show)
@@ -86,5 +94,34 @@ instance (Semigroup a, Semigroup b) => Semigroup (AccumulateBoth a b) where
     (<>) (AccumulateBoth (Failure a)) _           = AccumulateBoth (Failure a)
     (<>) _           (AccumulateBoth (Failure b)) = AccumulateBoth (Failure b)
 
-semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
-semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+
+----------------------------------------- 8 ----------------------
+
+newtype Mem s a =
+    Mem {
+        runMem :: s -> (a,s)
+    }
+instance Semigroup a => Semigroup (Mem s a) where
+    (<>) (Mem a) (Mem b) = Mem ( \s ->
+        let (a1,s') = a s 
+            (a2,s'') = b s'
+        in (a1<>a2, s'')
+        )
+
+instance Monoid a => Monoid (Mem s a) where
+    mempty :: Monoid a => Mem s a
+    mempty = Mem (\s -> (mempty,s))
+    mappend = (<>)  
+
+main :: IO ()
+main = do
+    let f' = Mem (\s -> ("Hi", s + 1))
+    let g' = Mem (\s -> (" there!", s + 9000)) -- Adds 9000 to state
+    
+    let combined = f' <> g' -- Should combine strings and add numbers
+    
+    print $ runMem combined 0
+    -- Expected: ("Hi there!", 9001)
+    
+    -- Expected: ("", 0)
