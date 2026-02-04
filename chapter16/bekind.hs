06de038f8a72435c686be789240c9547273e45b9
Nawf Abdullah <k.nawfabdullah@gmail.com>
2026-02-04 16:38:59 +0530

    chapter 16 started

---
+++ chapter16/bekind.hs
@@ -0,0 +1,11 @@
+-- what is the kind of a? a -> a
+-- *
+
+-- Qhat are the kinds of b and T?
+-- a -> b a -> T (b a)
+-- b : * -> *
+-- T: * -> *
+
+-- 3. What’s the kind of c?
+-- c a b -> c b a
+-- * -> * -> *
