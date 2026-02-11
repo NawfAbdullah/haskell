27f500cd627f6e72567adec517cc317daf323225
Nawf Abdullah <k.nawfabdullah@gmail.com>
2026-02-11 16:04:14 +0530

    chapter 16

--- chapter16/chapterExercises.hs
+++ chapter16/chapterExercises.hs
@@ -62,3 +62,55 @@ instance Functor (Flip K' a) where
 --------------------------- 4 ---------------------------
 data EvilGoateeConst a b = GoatyConst b deriving (Eq, Show)
 
+instance Functor (EvilGoateeConst a) where 
+    fmap f (GoatyConst b) = GoatyConst (f b)
+
+--------------------------- 5 ---------------------------
+
+data LiftItOut f a = LiftItOut (f a) deriving (Eq, Show)
+
+instance Functor f => Functor (LiftItOut f) where
+    fmap f (LiftItOut fa) = LiftItOut (fmap f fa)
+
+--------------------------- 6 ---------------------------
+data Parappa f g a = DaWrappa (f a) (g a) deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+    fmap f (DaWrappa fa ga) = DaWrappa (fmap f fa) (fmap f ga)
+
+--------------------------- 7 ---------------------------
+
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b)
+instance (Functor f, Functor g) => Functor (IgnoreOne f g a) where
+    fmap f (IgnoringSomething fa gb) = IgnoringSomething fa (fmap f gb)
+
+
+--------------------------- 8 ---------------------------
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+
+instance Functor g => Functor (Notorious g o a) where 
+
+    fmap f (Notorious go ga gt) = Notorious go ga (fmap f gt)
+
+--------------------------- 9 ---------------------------
+data List a = Nil | Cons a (List a)
+
+instance Functor List where
+    fmap f Nil = Nil 
+    fmap f (Cons a as) = Cons (f a) (fmap f as)
+
+--------------------------- 10 ---------------------------
+data GoatLord a = NoGoat | OneGoat a | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+
+instance Functor GoatLord where
+    fmap f NoGoat = NoGoat
+    fmap f (OneGoat a) = OneGoat (f a)
+    fmap f (MoreGoats ga gb gc) = MoreGoats (fmap f ga) (fmap f gb) (fmap f gc)
+
+--------------------------- 11 ---------------------------
+data TalkToMe a = Halt | Print String a | Read (String -> a)
+instance Functor TalkToMe where
+    fmap f Halt = Halt
+    fmap f (Print b a) =  Print b (f a)
+    fmap f (Read g) = Read (\s -> f (g s))
+    
